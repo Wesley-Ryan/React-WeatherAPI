ceca398c2966474f1f4f39493d31d9c5b4f28588
Wesley-Ryan <im.wesleyryan@gmail.com>
2020-10-08 15:53:57 -0700

    Fetching from weather api,  only showing active weather

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useForm } from "react-hook-form";
+import { Button } from "reactstrap";
+import { API_KEY } from "./utils";
+import Weather from "./components/Weather";
 import "./App.css";
 
 function App() {
+  const [weather, setWeather] = useState([]);
+  const { register, handleSubmit, reset } = useForm();
+  const [location, setLocation] = useState(80239);
+
+  const onSubmit = (data) => {
+    setLocation(data.zipcode);
+    reset();
+  };
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?zip=${location},us&appid=${API_KEY}&units=imperial`
+      )
+      .then((res) => {
+        const data = res.data;
+        setWeather({
+          data: data,
+          city: data.name,
+          temperature: data.main.temp,
+          realFeel: data.main.feels_like,
+          description: data.weather[0].description,
+          icon: data.weather[0].icon,
+          error: "",
+        });
+      })
+
+      .catch((err) => console.log(err));
+  }, [location]);
+
+  //form
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title ">Weather Guide</h1>
+      <div className="container">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="input-container ">
+            <input name="zipcode" placeholder="Zipcode" ref={register} />
+
+            <Button className="btn" color="primary" type="submit">
+              Submit
+            </Button>
+            <h5>Enter a zipcode to check the weather</h5>
+          </div>
+        </form>
+        <Weather
+          temp={weather.temperature}
+          feel={weather.realFeel}
+          city={weather.city}
+          icon={weather.icon}
+          description={weather.description}
+        />
+      </div>
     </div>
   );
 }
---
+++ src/utils/index.js
@@ -0,0 +1,1 @@
+export const API_KEY = "0faf43864ae9fb501559fd2c33a053fe";
